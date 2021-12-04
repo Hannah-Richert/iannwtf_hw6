0ce6a64970d0182f30ca216cfe67b4f3d59103b9
leonl42 <noinnick8@gmail.com>
2021-12-04 11:12:41 +0100

    Delete rfield files

--- main.py
+++ main.py
@@ -4,14 +4,14 @@ from util import load_data, test, visualize
 from SimpleModel import MyModel
 import numpy as np
 from classify import classify
-from receptive_field import ReceptiveField
 from ResNet import ResNet
 from DenseNet import DenseNet
 
 tf.keras.backend.clear_session()
 
 train_ds, valid_ds, test_ds = load_data()
-optimizer = tf.keras.optimizers.Adam(0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07)
+optimizer = tf.keras.optimizers.Adam(
+    0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07)
 
 #small_models = [ResNet(block_filters = [16,24,32],blocks = 2),DenseNet(filters=6,blocks=2,block_rep=3)]
 #big_models = [ResNet(block_filters = [8,8,8,12,12,12],out_filters=[16,16,16,24,24,24],blocks = 6),]
@@ -25,7 +25,8 @@ optimizer = tf.keras.optimizers.Adam(0.001, beta_1=0.9, beta_2=0.999, epsilon=1e
 # parameters(trainable) = 248,522 / accuracy (valid_ds; 10 epochs) = 59,4%
 #models = [ResNet(block_filters = [32,64,128,32], blocks = 3)]
 
-models = [ResNet(block_filters = [8,8,12,24],out_filters=[16,16,24,24],modes = ["normal","strifed","normal","constant"],blocks = 4)]
+models = [ResNet(block_filters=[8, 8, 12, 24], out_filters=[16, 16, 24, 24], modes=[
+                 "normal", "strifed", "normal", "constant"], blocks=4)]
 #models = [DenseNet(filters=4,blocks=3,block_rep=[2,6,4])]
 
 
@@ -35,7 +36,6 @@ models = [ResNet(block_filters = [8,8,12,24],out_filters=[16,16,24,24],modes = [
 #models = [DenseNet(filters=4, blocks=3, block_rep=8)]
 # parameters(trainable) = 91,306  / accuracy (valid_ds; 10 epochs) = 64,6%
 #models = [DenseNet(filters=12,blocks=3,block_rep=4)]
-
 
 
 # calculating receptive field
@@ -51,7 +51,8 @@ models = [ResNet(block_filters = [8,8,12,24],out_filters=[16,16,24,24],modes = [
 with tf.device('/device:gpu:0'):
     # training the model
     for model in models:
-        results, trained_model = classify(model, optimizer, 0, train_ds, valid_ds)
+        results, trained_model = classify(
+            model, optimizer, 0, train_ds, valid_ds)
 
         # testing the trained model
         # (this code snippet should only be inserted when one decided on all hyperparameters)
@@ -59,4 +60,4 @@ with tf.device('/device:gpu:0'):
         #print("Accuracy (test set):", test_accuracy)
 
         # visualizing losses and accuracy
-        visualize(results[0],results[1],results[2])
+        visualize(results[0], results[1], results[2])
